f4a8201c91cc39b9093d3ea9763496bdfd2cedbd
howtoautomateinth <mart@howtoautomate.in.th>
2020-06-27 23:02:47 +0700

    SAM integration with SNS

---
+++ SNS/sns-lambda-integration/src/handlers/sns-payload-logger.js
@@ -0,0 +1,9 @@
+/**
+ * A Lambda function that logs the payload received from SNS.
+ */
+exports.snsPayloadLoggerHandler = async (event, context) => {
+    // All log statements are written to CloudWatch by default. For more information, see
+    // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
+   
+    console.info("EVENT\n" + JSON.stringify(event, null, 2))
+}
