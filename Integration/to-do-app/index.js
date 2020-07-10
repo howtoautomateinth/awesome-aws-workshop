ac74067b391c78bf7683c2e3c6ae42c34f4f667c
Howtoautomate.in.th <mart@howtoautomate.in.th>
2020-07-10 13:43:00 +0700

    todolist: template for add logic todo app

--- Integration/to-do-app/index.js
+++ Integration/to-do-app/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+
 const app = express()
+app.set("view engine", "ejs");
+app.use("/static",express.static(__dirname + '/static'))
+
 var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('index.ejs'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
