d33fc0cf77a029c205680a97059284076548ca8f
Howtoautomate.in.th <mart@howtoautomate.in.th>
2020-07-12 09:43:46 +0700

    todolist: simple render data on view

--- Integration/to-do-app/index.js
+++ Integration/to-do-app/index.js
@@ -1,14 +1,46 @@
-const express = require('express')
-const config = require('config')
+const express = require("express");
+const config = require("config");
+const AWS = require("aws-sdk");
+const app = express();
+const dynamodb = null;
 
-const app = express()
-const appConfig = config.get('dbConfig');
+if (config.has("LocalDbConfig")) {
+  const appConfig = config.get("LocalDbConfig");
+  console.log("=== Set database to local ===");
+} else if (config.has("DbConfig")) {
+  dynamodb = new AWS.DynamoDB();
+  console.log("=== Database witll base on your aws credential ===");
+}
 
 app.set("view engine", "ejs");
-app.use("/static",express.static(__dirname + '/static'))
+app.use("/static", express.static(__dirname + "/static"));
 
 var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.render('index.ejs'))
+async function getAllToDoList() {
+  return [
+    {
+      header: "Header 1",
+      content: "Content 1",
+    },
+    {
+      header: "Header 2",
+      content: "Content 2",
+    },
+    {
+      header: "Header 3",
+      content: "Content 3",
+    },
+  ];
+}
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get("/", async function (req, res) {
+  let todolist = await getAllToDoList();
+  res.render("index.ejs", {
+    todolist: todolist,
+  });
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
