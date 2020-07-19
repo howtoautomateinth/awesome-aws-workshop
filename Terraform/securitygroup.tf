474539f270c3ae4b7de9e46b222b80ca7f23c4ba
howtoautomateinth <mart@howtoautomate.in.th>
2020-07-19 21:45:23 +0700

    TF: security group for infrastructure

---
+++ Terraform/securitygroup.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "hta-sg" {
+  vpc_id      = aws_vpc.hta-vpc-main.id
+  name        = "hta-sg"
+  description = "security group that allows nodejs,ssh and all egress traffic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "hta-sg"
+  }
+}
+
