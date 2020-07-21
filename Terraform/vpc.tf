e0b1f5833139ae85fd63482c88fcc9861dfa03ac
howtoautomateinth <mart@howtoautomate.in.th>
2020-07-21 14:57:38 +0700

    TF: update vpc infrastructure

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -22,10 +22,21 @@ resource "aws_subnet" "public-subnet-1" {
   }
 }
 
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id                  = aws_vpc.hta-vpc-main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-1b"
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
 # Private Subnets
 resource "aws_subnet" "private-subnet-1" {
   vpc_id                  = aws_vpc.hta-vpc-main.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "us-east-1b"
 
